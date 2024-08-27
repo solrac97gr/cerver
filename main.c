e15267c3d55156c789d5efa8cc5b6f3453572698
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2024-08-27 21:05:18 +0200

    change the <> for double quotes

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
-#include <cerver.h>
+#include "cerver.h"
 
 void HelloWorld(Request *request, Response *response) {
-    add_header(response, "Content-Type", "application/json");
-    response->body = strdup("{\"message\": \"Hello, World!\"}");
+  add_header(response, "Content-Type", "application/json");
+  response->body = strdup("{\"message\": \"Hello, World!\"}");
 }
 
 void EchoHandler(Request *request, Response *response) {
-    add_header(response, "Content-Type", "application/json");
-    if (request->body) {
-        // Directly set the response body to the request body
-        response->body = strdup(request->body);
-    } else {
-        response->body = strdup("{\"body\":\"No body received\"}");
-    }
+  add_header(response, "Content-Type", "application/json");
+  if (request->body) {
+    // Directly set the response body to the request body
+    response->body = strdup(request->body);
+  } else {
+    response->body = strdup("{\"body\":\"No body received\"}");
+  }
 }
 
-int main(int argc,char *argv[])
-{
-    Cerver *cerver = NewCerver(8080);
-    cerver->route_register(cerver, "/", "GET", HelloWorld);
-    cerver->route_register(cerver, "/echo", "POST", EchoHandler); // Register EchoHandler for POST /echo
-    cerver->start(cerver);
-    return 0;
+int main(int argc, char *argv[]) {
+  Cerver *cerver = NewCerver(8080);
+  cerver->route_register(cerver, "/", "GET", HelloWorld);
+  cerver->route_register(cerver, "/echo", "POST",
+                         EchoHandler); // Register EchoHandler for POST /echo
+  cerver->start(cerver);
+  return 0;
 }
